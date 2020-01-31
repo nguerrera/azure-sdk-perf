dcd646e5c98b9842bcf78c20d39879df28fbc33d
Mike Harder <mharder@microsoft.com>
2020-01-30 17:36:15 -0800

    Add test for HttpClient.GetStringAsync()

---
+++ net/Azure.Test.PerfStress/UrlOptions.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace Azure.Test.PerfStress
+{
+    public class UrlOptions : PerfStressOptions
+    {
+        [Option('u', "url", Required = true)]
+        public string Url { get; set; }
+    }
+}
