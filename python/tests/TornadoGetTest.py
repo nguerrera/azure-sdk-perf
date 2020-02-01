cf9740c4c40d70d78834052e2cf7782c2c052c83
Mike Harder <mharder@microsoft.com>
2020-02-01 02:10:22 +0000

    Add test for tornado http client

---
+++ python/tests/TornadoGetTest.py
@@ -0,0 +1,16 @@
+from tornado import httpclient
+
+from core.PerfStressTest import PerfStressTest
+
+class TornadoGetTest(PerfStressTest):
+
+    async def GlobalSetupAsync(self):
+        httpclient.AsyncHTTPClient.configure("tornado.curl_httpclient.CurlAsyncHTTPClient")
+        type(self).client = httpclient.AsyncHTTPClient()
+
+    async def RunAsync(self):
+        await type(self).client.fetch(self.Arguments.url)
+
+    @staticmethod
+    def AddArguments(parser):
+        parser.add_argument('-u', '--url', required=True)
