67ba393aec800d325f342d16240de68c5dbfaa57
Mike Harder <mharder@microsoft.com>
2020-03-24 14:46:56 -0700

    Receive message and signal completion

---
+++ PerfStressDriver/DevOpsServiceBusMessage.cs
@@ -0,0 +1,93 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PerfStressDriver
+{
+    class DevOpsServiceBusMessage
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        private readonly ServiceBusReceivedMessage _message;
+
+        public DevOpsServiceBusMessage(ServiceBusReceivedMessage message)
+        {
+            _message = message;
+        }
+
+        public string Body => Deserialize<string>(_message.Body.ToArray());
+
+        public string PlanUrl => (string)_message.Properties["PlanUrl"];
+        public string ProjectId => (string)_message.Properties["ProjectId"];
+        public string HubName => (string)_message.Properties["HubName"];
+        public string PlanId => (string)_message.Properties["PlanId"];
+        public string JobId => (string)_message.Properties["JobId"];
+        public string TimelineId => (string)_message.Properties["TimelineId"];
+        public string TaskInstanceName => (string)_message.Properties["TaskInstanceName"];
+        public string TaskInstanceId => (string)_message.Properties["TaskInstanceId"];
+        public string AuthToken => (string)_message.Properties["AuthToken"];
+
+        public async Task SignalCompletion(bool succeeded)
+        {
+            // https://docs.microsoft.com/en-us/azure/devops/pipelines/tasks/utility/publish-to-azure-service-bus?view=azure-devops#where-should-a-task-signal-completion
+            // https://github.com/microsoft/azure-pipelines-extensions/blob/master/ServerTaskHelper/HttpRequestSampleWithoutHandler/MyApp.cs#L258
+
+            var url = $"{PlanUrl}/{ProjectId}/_apis/distributedtask/hubs/{HubName}/plans/{PlanId}/events?api-version=2.0-preview.1";
+
+            var body = new
+            {
+                name = "TaskCompleted",
+                taskId = TaskInstanceId,
+                jobId = JobId,
+                result = succeeded ? "succeeded" : "failed",
+            };
+
+            var content = new ByteArrayContent(JsonSerializer.SerializeToUtf8Bytes(body));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var message = new HttpRequestMessage(HttpMethod.Post, url);
+            message.Headers.Authorization = new AuthenticationHeaderValue("Basic",
+                Convert.ToBase64String(Encoding.ASCII.GetBytes(":" + AuthToken)));
+            message.Content = content;
+
+            var response = await _httpClient.SendAsync(message);
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        private static T Deserialize<T>(byte[] data)
+        {
+            var serializer = new DataContractSerializer(typeof(T));
+            using (var stream = new MemoryStream(data))
+            using (var reader = XmlDictionaryReader.CreateBinaryReader(stream, XmlDictionaryReaderQuotas.Max))
+            {
+                return (T)serializer.ReadObject(reader);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Properties");
+            foreach (var p in _message.Properties)
+            {
+                sb.AppendLine($"  {p.Key}: {p.Value}");
+            }
+
+            sb.AppendLine();
+            sb.AppendLine("Body");
+            sb.AppendLine(Body);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ PerfStressDriver/Program.cs
@@ -0,0 +1,64 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace PerfStressDriver
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("SERVICE_BUS_CONNECTION_STRING");
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Undefined environment variable SERVICE_BUS_CONNECTION_STRING");
+            }
+
+            var client = new ServiceBusClient(connectionString);
+            var processor = client.GetProcessor("jobs", new ServiceBusProcessorOptions
+            {
+                // https://github.com/Azure/azure-sdk-for-net/issues/10797
+                AutoComplete = false
+            });
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            processor.ProcessMessageAsync += async args =>
+            {
+                var message = args.Message;
+                var devOpsMessage = new DevOpsServiceBusMessage(message);
+
+                Console.WriteLine(devOpsMessage);
+
+                try
+                {
+                    await devOpsMessage.SignalCompletion(succeeded: true);
+                    await args.Receiver.CompleteAsync(message);
+                }
+                catch
+                {
+                    await args.Receiver.AbandonAsync(message);
+                }
+
+                tcs.SetResult(true);
+            };
+
+            processor.ProcessErrorAsync += args =>
+            {
+                throw args.Exception;
+            };
+
+            await processor.StartProcessingAsync();
+            await tcs.Task;
+            await processor.StopProcessingAsync();
+        }
+
+    }
+}
