428cb3ea2eb1b38801448548c53bf2f224de91a1
Mike Harder <mharder@microsoft.com>
2020-05-05 16:11:58 -0700

    Add test for BlockBlobClient.StageBlock()

---
+++ net/Azure.Storage.Blobs.PerfStress/StageBlockTest.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Storage.Blobs.PerfStress.Core;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azure.Storage.Blobs.PerfStress
+{
+    public class StageBlockTest : RandomBlobTest<StorageTransferOptionsOptions>
+    {
+        private readonly string _blockId = Guid.NewGuid().ToString("N");
+
+        public StageBlockTest(StorageTransferOptionsOptions options) : base(options)
+        {
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+            using var stream = RandomStream.Create(Options.Size);
+            BlockBlobClient.StageBlock(_blockId, stream);
+        }
+
+        public override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            using var stream = RandomStream.Create(Options.Size);
+            await BlockBlobClient.StageBlockAsync(_blockId, stream);
+        }
+    }
+}
