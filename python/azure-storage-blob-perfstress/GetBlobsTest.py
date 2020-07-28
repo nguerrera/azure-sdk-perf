4d36bfa95451e87a85d7aa0264b7269d2f40b5ec
Mike Harder <mharder@microsoft.com>
2020-07-27 20:05:47 -0700

    Change base class to _ContainerTest

--- python/azure-storage-blob-perfstress/GetBlobsTest.py
+++ python/azure-storage-blob-perfstress/GetBlobsTest.py
@@ -1,52 +1,23 @@
-import os
+import uuid
 
-from azure.test.perfstress import PerfStressTest
+from _ContainerTest import _ContainerTest
 
-from azure.storage.blob import ContainerClient as SyncContainerClient
-from azure.storage.blob.aio import ContainerClient as AsyncContainerClient
-
-class GetBlobsTest(PerfStressTest):
-    '''This test evaluates the perf of enumerating blobs'''
-    container_name = PerfStressTest.NewGuid()
-    def __init__(self):
-        connection_string = os.environ.get("STORAGE_CONNECTION_STRING")
-        if not connection_string:
-            raise Exception("Undefined environment variable STORAGE_CONNECTION_STRING")
-
-        self.container_client = SyncContainerClient.from_connection_string(conn_str=connection_string, container_name=self.container_name)
-
-        #TODO: I really hate this.
-        self.async_container_client = AsyncContainerClient.from_connection_string(conn_str=connection_string, container_name=self.container_name)
-
-
-    async def SetupAsync(self):
-        await self.async_container_client.__aenter__()
-
-
-    async def CleanupAsync(self):
-        await self.async_container_client.__aexit__()
-
-
+class GetBlobsTest(_ContainerTest):
     async def GlobalSetupAsync(self):
-        self.container_client.create_container()
+        await super().GlobalSetupAsync()
+        # TODO: Upload in parallel to improve setup perf
         for _ in range(0, self.Arguments.count): #pylint: disable=no-member
-            self.container_client.upload_blob(PerfStressTest.NewGuid(), '')
-
-
-    async def GlobalCleanupAsync(self):
-        self.container_client.delete_container()
-
+            self.container_client.upload_blob("getblobstest-" + str(uuid.uuid4()), '')
 
     def Run(self):
         for _ in self.container_client.list_blobs():
             pass
 
-
     async def RunAsync(self):
         async for _ in self.async_container_client.list_blobs(): #pylint: disable=not-an-iterable
             pass
 
-
     @staticmethod
     def AddArguments(parser):
+        super(GetBlobsTest, GetBlobsTest).AddArguments(parser)
         parser.add_argument('-c', '--count', nargs='?', type=int, help='Number of blobs to populate.  Default is 1.', default=1)
