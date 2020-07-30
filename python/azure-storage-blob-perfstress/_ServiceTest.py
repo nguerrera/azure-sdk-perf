3de253b6b1900ce58939a3b8eb2bd60cc59d567c
Mike Harder <mharder@microsoft.com>
2020-07-29 17:26:22 -0700

    Add argument "service-client-per-instance"

--- python/azure-storage-blob-perfstress/_ServiceTest.py
+++ python/azure-storage-blob-perfstress/_ServiceTest.py
@@ -6,7 +6,9 @@ from azure.storage.blob import BlobServiceClient as SyncBlobServiceClient
 from azure.storage.blob.aio import BlobServiceClient as AsyncBlobServiceClient
 
 class _ServiceTest(PerfStressTest):
-    # TODO: Move service_client from instance to static variable
+    service_client = None
+    async_service_client = None
+
     def __init__(self, arguments):
         super().__init__(arguments)
 
@@ -14,12 +16,16 @@ class _ServiceTest(PerfStressTest):
         if not connection_string:
             raise Exception("Undefined environment variable STORAGE_CONNECTION_STRING")
 
-        if self.Arguments.max_single_put_size:
-            self.service_client = SyncBlobServiceClient.from_connection_string(conn_str=connection_string, max_single_put_size=self.Arguments.max_single_put_size)
-            self.async_service_client = AsyncBlobServiceClient.from_connection_string(conn_str=connection_string, max_single_put_size=self.Arguments.max_single_put_size)
-        else:
-            self.service_client = SyncBlobServiceClient.from_connection_string(conn_str=connection_string)
-            self.async_service_client = AsyncBlobServiceClient.from_connection_string(conn_str=connection_string)
+        if not _ServiceTest.service_client or self.Arguments.service_client_per_instance:
+            if self.Arguments.max_single_put_size:
+                _ServiceTest.service_client = SyncBlobServiceClient.from_connection_string(conn_str=connection_string, max_single_put_size=self.Arguments.max_single_put_size)
+                _ServiceTest.async_service_client = AsyncBlobServiceClient.from_connection_string(conn_str=connection_string, max_single_put_size=self.Arguments.max_single_put_size)
+            else:
+                _ServiceTest.service_client = SyncBlobServiceClient.from_connection_string(conn_str=connection_string)
+                _ServiceTest.async_service_client = AsyncBlobServiceClient.from_connection_string(conn_str=connection_string)
+
+        self.service_client = _ServiceTest.service_client
+        self.async_service_client =_ServiceTest.async_service_client
 
     async def CloseAsync(self):
         await self.async_service_client.close()
@@ -29,3 +35,4 @@ class _ServiceTest(PerfStressTest):
     def AddArguments(parser):
         super(_ServiceTest, _ServiceTest).AddArguments(parser)
         parser.add_argument('--max-single-put-size', nargs='?', type=int, help='Maximum size of blob uploading in single HTTP PUT.  Default is None.')
+        parser.add_argument('--service-client-per-instance', action='store_true', help='Create one ServiceClient per test instance.  Default is to share a single ServiceClient.', default=False)
