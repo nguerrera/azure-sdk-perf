9917173a8af74da52db40fd537d192695dca60be
Mike Harder <mharder@microsoft.com>
2020-09-22 12:38:00 -0700

    Rename timer tests

---
+++ net/System.PerfStress/TimerTestBase.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Test.PerfStress;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace System.PerfStress
+{
+    public class TimerTestBase : PerfStressTestBase<PerfStressOptions>
+    {
+        public TimerTestBase(PerfStressOptions options) : base(options)
+        {
+        }
+
+        private Timer CreateTimer(ResultCollector resultCollector, bool latency)
+        {
+            var sw = Stopwatch.StartNew();
+
+            return new Timer(_ =>
+            {
+                if (latency)
+                {
+                    resultCollector.Add(sw.Elapsed);
+                    sw.Restart();
+                }
+                else
+                {
+                    resultCollector.Add();
+                }
+            },
+            state: null, dueTime: TimeSpan.FromSeconds(1), period: TimeSpan.FromSeconds(1));
+        }
+
+        public override void RunLoop(ResultCollector resultCollector, bool latency, Channel<(TimeSpan, Stopwatch)> pendingOperations, CancellationToken cancellationToken)
+        {
+            using var timer = CreateTimer(resultCollector, latency);
+            cancellationToken.WaitHandle.WaitOne();
+        }
+
+        public override async Task RunLoopAsync(ResultCollector resultCollector, bool latency, Channel<(TimeSpan, Stopwatch)> pendingOperations, CancellationToken cancellationToken)
+        {
+            using var timer = CreateTimer(resultCollector, latency);
+            await Task.Delay(Timeout.InfiniteTimeSpan, cancellationToken);
+        }
+    }
+}
