dd8d305748d2272a4c565e08c615eeee2e5ebe9b
Mike Harder <mharder@microsoft.com>
2020-09-25 17:55:56 -0700

    Ensure test doesn't leave message in queue

--- net/Azure.Messaging.EventHubs.PerfStress/EventProcessorTest.cs
+++ net/Azure.Messaging.EventHubs.PerfStress/EventProcessorTest.cs
@@ -24,7 +24,8 @@ namespace Azure.Messaging.EventHubs.PerfStress
         public override async Task RunAsync(ResultCollector resultCollector, bool latency, Channel<(TimeSpan, Stopwatch)> pendingOperations, CancellationToken cancellationToken)
         {
             var sw = Stopwatch.StartNew();
-            var semaphore = new SemaphoreSlim(initialCount: 1, maxCount: 1);
+            
+            using var sendSemaphore = new SemaphoreSlim(initialCount: 1, maxCount: 1);
 
             Processor.ProcessEventAsync += e =>
             {
@@ -36,32 +37,49 @@ namespace Azure.Messaging.EventHubs.PerfStress
                 }
 
                 var start = BitConverter.ToInt64(e.Data.Body.ToArray(), 0);
-
-                resultCollector.Add(TimeSpan.FromTicks(end - start));
+                resultCollector.Add(latency: TimeSpan.FromTicks(end - start));
 
                 // Allow producer to send next message
-                semaphore.Release();
+                sendSemaphore.Release();
 
                 return Task.CompletedTask;
             };
 
-            await Processor.StartProcessingAsync(cancellationToken);
-
-            while (!cancellationToken.IsCancellationRequested)
+            try
             {
-                // Don't send a new message until processor has handled the previous message
-                await semaphore.WaitAsync(cancellationToken);
+                await Processor.StartProcessingAsync(cancellationToken);
 
-                var start = sw.ElapsedTicks;
-                using (var batch = await Producer.CreateBatchAsync(cancellationToken))
+                while (!cancellationToken.IsCancellationRequested)
                 {
-                    batch.TryAdd(new EventData(BitConverter.GetBytes(start)));
-                    await Producer.SendAsync(batch, cancellationToken);
-                }
-            }
+                    // Don't send a new message until processor has received the previous message.
+                    await sendSemaphore.WaitAsync(cancellationToken);
 
-            // Do not flow cancellationToken, since we want processing to stop gracefully even after the test run is cancelled
-            await Processor.StopProcessingAsync();
+                    try
+                    {
+                        using var batch = await Producer.CreateBatchAsync(cancellationToken);
+
+                        var start = sw.ElapsedTicks;
+                        batch.TryAdd(new EventData(BitConverter.GetBytes(start)));
+
+                        await Producer.SendAsync(batch, cancellationToken);
+                    }
+                    catch
+                    {
+                        // Message was not actually sent, so reset sendSemaphore to allow test to terminate.
+                        sendSemaphore.Release();
+                        throw;
+                    }
+                }
+
+                // We never want to end test with a message in the queue (even if the test is cancelled), so we wait until the last
+                // sent message has been received (and do not flow cancellationToken).
+                await sendSemaphore.WaitAsync();
+            }
+            finally
+            {
+                // Do not flow cancellationToken, since we want processing to stop gracefully even after the test run is cancelled
+                await Processor.StopProcessingAsync();
+            }
         }
     }
 }
