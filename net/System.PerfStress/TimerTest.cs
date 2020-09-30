6c80cf545f00dbee462529bafd288cc629475310
Mike Harder <mharder@microsoft.com>
2020-09-30 16:08:02 -0700

    Revert "Rename timer tests"
    This reverts commit 9917173a8af74da52db40fd537d192695dca60be.

--- net/System.PerfStress/TimerTest.cs
+++ net/System.PerfStress/TimerTest.cs
@@ -1,31 +1,47 @@
 ﻿using Azure.Test.PerfStress;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace System.PerfStress
 {
-    public class TimerTest : PerfStressTest<PerfStressOptions>
+    public class TimerTest : PerfStressTestBase<PerfStressOptions>
     {
-        private readonly SemaphoreSlim _semaphoreSlim;
-
-        [SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>")]
-        private readonly Timer _timer;        
-
         public TimerTest(PerfStressOptions options) : base(options)
         {
-            _semaphoreSlim = new SemaphoreSlim(0);
-            _timer = new Timer(_ => _semaphoreSlim.Release(), state: null, dueTime: TimeSpan.FromSeconds(1), period: TimeSpan.FromSeconds(1));
-        }
-        
-        public override void Run(CancellationToken cancellationToken)
-        {
-            _semaphoreSlim.Wait();
         }
 
-        public override Task RunAsync(CancellationToken cancellationToken)
+        private Timer CreateTimer(ResultCollector resultCollector, bool latency)
         {
-            return _semaphoreSlim.WaitAsync();
+            var sw = Stopwatch.StartNew();
+
+            return new Timer(_ =>
+            {
+                if (latency)
+                {
+                    resultCollector.Add(sw.Elapsed);
+                    sw.Restart();
+                }
+                else
+                {
+                    resultCollector.Add();
+                }
+            },
+            state: null, dueTime: TimeSpan.FromSeconds(1), period: TimeSpan.FromSeconds(1));
+        }
+
+        public override void RunLoop(ResultCollector resultCollector, bool latency, Channel<(TimeSpan, Stopwatch)> pendingOperations, CancellationToken cancellationToken)
+        {
+            using var timer = CreateTimer(resultCollector, latency);
+            cancellationToken.WaitHandle.WaitOne();
+        }
+
+        public override async Task RunLoopAsync(ResultCollector resultCollector, bool latency, Channel<(TimeSpan, Stopwatch)> pendingOperations, CancellationToken cancellationToken)
+        {
+            using var timer = CreateTimer(resultCollector, latency);
+            await Task.Delay(Timeout.InfiniteTimeSpan, cancellationToken);
         }
     }
 }
