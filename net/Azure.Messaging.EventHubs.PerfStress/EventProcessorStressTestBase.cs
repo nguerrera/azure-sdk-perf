b6e6a81c83ed52a0a9b8cfad7d267e6a36b526ee
Mike Harder <mharder@microsoft.com>
2020-09-29 17:02:33 -0700

    Add EventProcessorStressTestBase

---
+++ net/Azure.Messaging.EventHubs.PerfStress/EventProcessorStressTestBase.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Messaging.EventHubs.PerfStress.Core;
+using Azure.Test.PerfStress;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Azure.Messaging.EventHubs.PerfStress
+{
+    public class EventProcessorStressTestBase : ProducerProcessorTestBase<PerfStressOptions>
+    {
+        public EventProcessorStressTestBase(PerfStressOptions options) : base(options)
+        {
+        }
+
+        public override void Run(ResultCollector resultCollector, bool latency, Channel<(TimeSpan, Stopwatch)> pendingOperations, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task RunAsync(ResultCollector resultCollector, bool latency, Channel<(TimeSpan, Stopwatch)> pendingOperations, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
